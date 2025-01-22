ad3f74f81cf561c7e2cd26381af1aa4cd9f255a7
Noah S <nonososoviolin@gmail.com>
2025-01-22 23:42:56 +0000

    Update plugin versions and remove unused dependency
    Upgraded `polyglot-version-catalogs` and `foojay-resolver-convention` plugins to newer versions. Removed the unused `run-paper` dependency from the version catalog. Added `org.gradle.java.home` property to specify the JDK location.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("ca.stellardrift.polyglot-version-catalogs") version "6.0.1"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+    id("ca.stellardrift.polyglot-version-catalogs") version "6.1.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
 rootProject.name = "Plop"
